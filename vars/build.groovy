658265fdf9bd4e504a0c719f94f01d378e6e856e
Jagilinki Hemasundar <126693281+Hemasundar123@users.noreply.github.com>
2026-02-14 13:22:48 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def build(String ProjectName,String DockerUser,String ImageTag){
+def build(String ProjectName,String ImageTag){
   echo "This is a Builded code"
-  sh "docker build -t ${DockerUser}/${ProjectName}:${ImageTag} ."
+  sh "docker build -t ${ProjectName}:${ImageTag} ."
 }
