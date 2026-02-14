457451689d5f4611011e5fad109de8a37bff9df2
Jagilinki Hemasundar <126693281+Hemasundar123@users.noreply.github.com>
2026-02-14 12:17:54 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,5 +1,5 @@
-def git(string url,string branch){
+def git(String url,String branch){
   echo "Hi This is Notes-app"
-  git url: "url", branch:"branch"
+  git url: "${url}", branch:"${branch}"
   echo "Clone the Repo"
 }
