a786f5f5ead0b439933fd2039c132ced94762682
Jagilinki Hemasundar <126693281+Hemasundar123@users.noreply.github.com>
2026-02-14 13:38:01 +0530

    Rename build function to call for better clarity

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def build(String ProjectName,String ImageTag){
+def call(String ProjectName,String ImageTag){
   echo "This is a Builded code"
   sh "docker build -t ${ProjectName}:${ImageTag} ."
 }
